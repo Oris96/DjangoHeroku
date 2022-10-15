88f510472f9df8abddfa08f1c67b3e72fb3766bc
Oris96 <mishazot@gmail.com>
2022-10-15 17:07:41 +0300

    1 task

--- DjangoHerokuProject/NewTestPythonFile.py
+++ DjangoHerokuProject/NewTestPythonFile.py
@@ -1,4 +1,20 @@
-for counter in range(99):
-    print('Andrei lox')
+years_animals_collection = {2000: 'Дракон',
+                            2001: 'Змея',
+                            2002: 'Лошадь',
+                            2003: 'Овца',
+                            2004: 'Обезьяна',
+                            2005: 'Петух',
+                            2006: 'Собака',
+                            2007: 'Свинья',
+                            2008: 'Крыса',
+                            2009: 'Бык',
+                            2010: 'Тигр',
+                            2011: 'Заяц'}
 
-print('Naruto vs Sasuke AMV Linkin Park смотреть')
+input_year = int(input())
+
+for year in range(2000, 2012):
+    if not (year - input_year) % 12:
+        print(years_animals_collection[year])
+
+
