565d92a782bdfb175472c1ee72e337049797215b
Oris96 <mishazot@gmail.com>
2022-10-16 11:31:10 +0300

    joseph task

--- DjangoHerokuProject/NewTestPythonFile.py
+++ DjangoHerokuProject/NewTestPythonFile.py
@@ -1,6 +1,7 @@
-num = int(input())
-print(f'{num:,}')
+def joseph(n, k):
+    return (joseph(n - 1, k) + k - 1) % n + 1 if n > 1 else 1
 
 
-
-
+n = int(input())
+k = int(input())
+print(joseph(n, k))
