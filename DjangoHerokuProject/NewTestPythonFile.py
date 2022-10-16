bcac272dda44315f887db24c67d029948341dd70
Oris96 <mishazot@gmail.com>
2022-10-16 18:46:11 +0300

    Coordinats task

--- DjangoHerokuProject/NewTestPythonFile.py
+++ DjangoHerokuProject/NewTestPythonFile.py
@@ -1,7 +1,23 @@
-def joseph(n, k):
-    return (joseph(n - 1, k) + k - 1) % n + 1 if n > 1 else 1
+count = int(input())
+dots = [input() for i in range(count)]
+first = 0
+second = 0
+third = 0
+fourth = 0
 
+for dot in dots:
+    coordinats = dot.split(' ')
+    if '0' not in coordinats:
+        if eval(coordinats[0]) > 0 and eval(coordinats[-1]) > 0:
+            first += 1
+        elif eval(coordinats[0]) < 0 and eval(coordinats[-1]) < 0:
+            third += 1
+        elif eval(coordinats[0]) > 0:
+            fourth += 1
+        elif eval(coordinats[0]) < 0:
+            second += 1
 
-n = int(input())
-k = int(input())
-print(joseph(n, k))
+print(f'Первая четверть: {first}\n'
+      f'Вторая четверть: {second}\n'
+      f'Третья четверть: {third}\n'
+      f'Четвертая четверть: {fourth}')
