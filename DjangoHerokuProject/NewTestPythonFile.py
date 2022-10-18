f933c5d26147da4738bebfa2fb6ef6d426d4b98d
Oris96 <mishazot@gmail.com>
2022-10-18 23:15:23 +0300

    sum of numbeers

--- DjangoHerokuProject/NewTestPythonFile.py
+++ DjangoHerokuProject/NewTestPythonFile.py
@@ -1,1 +1,14 @@
-print(len(set(input().split(' '))))
+rows = int(input())
+numbers = [int(input()) for i in range(rows)]
+composition = int(input())
+result = 'НЕТ'
+
+while len(numbers) > 1:
+    comps = [numbers[0] * i for i in numbers[1:]]
+    if composition in comps:
+        result = 'ДА'
+        break
+    else:
+        numbers.pop(0)
+
+print(result)
