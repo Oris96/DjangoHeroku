48b3e8453180935e0fc527b19a7ba740fa9bb6d8
Oris96 <mishazot@gmail.com>
2022-10-24 22:08:13 +0300

    похуй первое попавшееся решение

--- DjangoHerokuProject/NewTestPythonFile.py
+++ DjangoHerokuProject/NewTestPythonFile.py
@@ -1,4 +1,13 @@
-from re import search
+alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+word = input()
+phrase = f'запретил букву'
 
-number = int(input())
-result = [print(i + 1, end=" ") for i in range(number) if search(r'a.*n.*t.*o.*n', input())]
+for i in range(len(alphabet)):
+    if alphabet[i] in list(word + phrase):
+        if phrase:
+            print(f'{word} {phrase} {alphabet[i]}'.strip())
+        else:
+            print(f'{word} {alphabet[i]}'.strip())
+        word = word.replace(alphabet[i], '')
+        phrase = phrase.replace(alphabet[i], '').strip()
+        
